fce4c051a42b6ade451b338599aa16bd49f76782
xiaooofang <107698436+xiaooofang@users.noreply.github.com>
2022-06-17 23:12:28 +0800

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,24 @@
+$(document).ready(()=>{
+    $("art-se").mouseover(()=>{
+      $("art-se").text("🎄");
+    });
+    
+    $("art-se").mouseup(()=>{
+      $("art-se").text("👻");
+    });
+  });
+
+  <script src="3.js"></script>
+
+  <article class="art-re">      
+			  <div class="button">
+				  <img src="https://stickershop.line-scdn.net/stickershop/v1/sticker/26308491/android/sticker.png" width="100%">
+			  </div>
+				 <div class="art-main">
+				  <h2 class="art-title">撒嬌兔的可愛日常</h2>
+				   <p><a href="https://store.line.me/stickershop/product/10258/zh-Hant" class="art-red-more" target="_blank" >了解更多</a></p>
+			  </div>
+		  </article>
+
+
+      
